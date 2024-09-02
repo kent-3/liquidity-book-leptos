7e9f94209967245362397abb3eb1dbcabc9c6e8b
kent-3 <100624004+kent-3@users.noreply.github.com>
2024-09-02 10:55:10 -0400

    delete secret.js stuff

--- src/secretjs/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod client;
-pub mod tests;
-pub mod tx;
-pub mod wallet;
-
-pub use client::{ClientOptionsBuilder, SecretNetworkClient};
-pub use tests::SecretJsTests;
-pub use tx::TxOptionsBuilder;
-pub use wallet::Wallet;
