3c02aaf9f319ae129949a56e95b2927512e04120
kent-3 <100624004+kent-3@users.noreply.github.com>
2024-10-19 13:41:59 -0400

    add blanket implementation for Querier trait

--- src/liquidity_book/mod.rs
+++ src/liquidity_book/mod.rs
@@ -2,50 +2,45 @@ pub mod constants;
 pub mod contract_interfaces;
 pub mod curves;
 
-use constants::addrs::{LB_FACTORY_CONTRACT, LB_PAIR_CONTRACT};
 use contract_interfaces::*;
 
+use crate::error::Error;
 use cosmwasm_std::ContractInfo;
 use rsecret::{query::compute::ComputeQuerier, secret_client::CreateQuerierOptions};
 use secretrs::utils::EnigmaUtils;
-use tonic_web_wasm_client::Client;
+use serde::Serialize;
+use std::sync::{Arc, LazyLock};
+use tonic_web_wasm_client::Client as WebWasmClient;
 
-use crate::error::Error;
-use crate::keplr::Keplr;
+// NOTE: LB stuff is currently locked to testnet.
 
 static CHAIN_ID: &str = "pulsar-3";
+
 static GRPC_URL: &str = "https://grpc.pulsar.scrttestnet.com";
+
+static WEB_WASM_CLIENT: LazyLock<WebWasmClient> =
+    LazyLock::new(|| WebWasmClient::new(GRPC_URL.to_string()));
+
+static ENIGMA_UTILS: LazyLock<Arc<EnigmaUtils>> = LazyLock::new(|| {
+    EnigmaUtils::new(None, CHAIN_ID)
+        .expect("Failed to create EnigmaUtils")
+        .into()
+});
+
+static COMPUTE_QUERIER: LazyLock<ComputeQuerier<WebWasmClient, EnigmaUtils>> =
+    LazyLock::new(|| ComputeQuerier::new(WEB_WASM_CLIENT.clone(), ENIGMA_UTILS.clone()));
 
 pub trait Querier {
     async fn do_query(&self, contract: &ContractInfo) -> Result<String, Error>;
 }
 
-impl Querier for lb_factory::QueryMsg {
+impl<T: Serialize + Send + Sync> Querier for T {
     async fn do_query(&self, contract: &ContractInfo) -> Result<String, Error> {
         let contract_address = &contract.address;
         let code_hash = &contract.code_hash;
-
-        let client = Client::new(GRPC_URL.to_string());
-        let encryption_utils = EnigmaUtils::new(None, CHAIN_ID).unwrap();
-        let compute = ComputeQuerier::new(client, encryption_utils.into());
         let query = self;
-        compute
-            .query_secret_contract(contract_address, code_hash, query)
-            .await
-            .map_err(Into::into)
-    }
-}
 
-impl Querier for lb_pair::QueryMsg {
-    async fn do_query(&self, contract: &ContractInfo) -> Result<String, Error> {
-        let contract_address = &contract.address;
-        let code_hash = &contract.code_hash;
-
-        let client = Client::new(GRPC_URL.to_string());
-        let encryption_utils = EnigmaUtils::new(None, CHAIN_ID).unwrap();
-        let compute = ComputeQuerier::new(client, encryption_utils.into());
-        let query = self;
-        compute
+        COMPUTE_QUERIER
             .query_secret_contract(contract_address, code_hash, query)
             .await
             .map_err(Into::into)
