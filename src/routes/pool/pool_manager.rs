b4f85aa09836b2fddb4875297f037727bbe8b13b
kent-3 <100624004+kent-3@users.noreply.github.com>
2025-01-17 12:11:07 -0500

    begin chart experiment

--- src/routes/pool/pool_manager.rs
+++ src/routes/pool/pool_manager.rs
@@ -31,6 +31,36 @@ mod remove_liquidity;
 pub use add_liquidity::AddLiquidity;
 pub use remove_liquidity::RemoveLiquidity;
 
+pub struct MyData {
+    pub x: f64,
+    pub y1: f64,
+    pub y2: f64,
+}
+
+use leptos_chartistry::*;
+
+#[component]
+pub fn Example(debug: Signal<bool>, data: Signal<Vec<MyData>>) -> impl IntoView {
+    let series = Series::new(|data: &MyData| data.x)
+        .bar(|data: &MyData| data.y1)
+        .bar(|data: &MyData| data.y2);
+    view! {
+        <Chart
+            aspect_ratio=AspectRatio::from_outer_height(300.0, 1.2)
+            debug=debug
+            series=series
+            data=data
+
+            left=TickLabels::aligned_floats()
+            inner=[
+                AxisMarker::left_edge().into_inner(),
+                AxisMarker::bottom_edge().into_inner(),
+                YGridLine::default().into_inner(),
+            ]
+        />
+    }
+}
+
 #[component]
 pub fn PoolManager() -> impl IntoView {
     info!("rendering <PoolManager/>");
