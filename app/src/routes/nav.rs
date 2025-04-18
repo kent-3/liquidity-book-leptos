2c1ee9367f7a11883a1a7fd202bd0abdfce7a66e
kent-3 <100624004+kent-3@users.noreply.github.com>
2025-04-17 23:29:50 -0400

    more css cleanup

--- app/src/routes/nav.rs
+++ app/src/routes/nav.rs
@@ -5,11 +5,26 @@ use leptos_router::components::A;
 pub fn Nav() -> impl IntoView {
     view! {
         <nav class="leading-tight flex flex-row items-center">
-            <A exact=true strict_trailing_slash=false href="/liquidity-book-leptos/trade">
+            <A
+                exact=true
+                strict_trailing_slash=false
+                href="/liquidity-book-leptos/trade"
+                attr:class="text-muted-foreground px-3 py-1.5 no-underline leading-none"
+            >
                 "Trade"
             </A>
-            <A href="/liquidity-book-leptos/pool">"Pool"</A>
-            <a href="https://kent-3.github.io/liquidity-book/docs/" target="_blank" rel="noopener">
+            <A
+                href="/liquidity-book-leptos/pool"
+                attr:class="text-muted-foreground px-3 py-1.5 no-underline leading-none"
+            >
+                "Pool"
+            </A>
+            <a
+                href="https://kent-3.github.io/liquidity-book/docs/"
+                target="_blank"
+                rel="noopener"
+                class="text-muted-foreground px-3 py-1.5 no-underline leading-none"
+            >
                 "Docs"
             </a>
         </nav>
